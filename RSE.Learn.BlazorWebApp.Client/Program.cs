9912916c3d4a8045f7f5a55f43898afe5f032763
J. Ward M. Perez <jay17fame@yahoo.com>
2025-02-27 20:28:16 +0800

    work in progress

--- RSE.Learn.BlazorWebApp.Client/Program.cs
+++ RSE.Learn.BlazorWebApp.Client/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.FluentUI.AspNetCore.Components;
 using MudBlazor.Services;
@@ -9,5 +10,29 @@ builder.Services.AddMudServices();
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddAuthenticationStateDeserialization();
+
+builder.Services.AddHttpClient("WeatherAPI", client => client.BaseAddress = new Uri("https://localhost:7055/"))
+    .AddHttpMessageHandler(sp =>
+    {
+        var handler = sp.GetRequiredService<AuthorizationMessageHandler>()
+            .ConfigureHandler(authorizedUrls: ["https://localhost:7055"]);
+
+        return handler;
+    });
+
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("WeatherAPI"));
+
+builder.Services.AddOidcAuthentication(options =>
+{
+    // Set Keycloak configuration here
+    options.ProviderOptions.Authority = "https://localhost:8080/realms/DonaideAPIRealm";
+    options.ProviderOptions.ClientId = "donaide-web-client";
+    options.ProviderOptions.ResponseType = "code";
+    options.ProviderOptions.DefaultScopes.Add("donaide_api_scope");
+
+    // Optional: If you have a redirect URL or specific configurations, you can specify those here
+    options.ProviderOptions.RedirectUri = builder.HostEnvironment.BaseAddress;
+    options.ProviderOptions.PostLogoutRedirectUri = builder.HostEnvironment.BaseAddress;
+});
 
 await builder.Build().RunAsync();
